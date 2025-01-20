a81eb77ade8f7270ded1bc7ec6a146f139edfea7
Nikhil Kulkarni <nikhilkulkarni2208@gmail.com>
2025-01-20 16:46:03 +0530

    import template

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
 import transporter from '../config/nodemailer.js';
-import { EMAIL_VERIFY_TEMPLATE, PASSWORD_RESET_TEMPLATE } from '../../frontend/src/assets/emailTemplates.js'
+import { EMAIL_VERIFY_TEMPLATE, PASSWORD_RESET_TEMPLATE } from '../config/emailTemplates.js'
 
 /**
  * Helper functions to create access and refresh tokens
